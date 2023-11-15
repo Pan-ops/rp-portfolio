9ce7a8e7f80983ab70e93692199c58f80c5c2b5b
Pannnn112 <70612529+Pan-ops@users.noreply.github.com>
2023-11-15 22:58:22 +1300

    Delete panhx.com directory

--- panhx.com/connect_panhx.ps1
+++
@@ -1,9 +0,0 @@
-# Set the path to your SSH key file
-$SSH_KEY = "C:\Users\pan_h\panhx.com\panhx_key.pem"
-
-# Set the username and IP address of the remote server
-$REMOTE_USER = "pan"
-$REMOTE_IP = "20.55.69.96"
-
-# SSH into the remote server using the specified key
-ssh -i $SSH_KEY "$REMOTE_USER@$REMOTE_IP"
